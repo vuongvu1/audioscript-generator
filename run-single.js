4bfc3c4a8bf62953eafa76e710832607edcdd063
vuongvu <vuong.vu@2359media.com>
2021-03-23 23:14:38 +0800

    Add run single file script

---
+++ run-single.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const child_process = require("child_process");
+const fs = require("fs");
+const fsExtra = require("fs-extra");
+
+const { INPUT_DIR, OUTPUT_DIR, API_KEY, API_URL } = process.env;
+
+const fileName = process.env.FILE_NAME;
+
+function runCmd(cmd) {
+  const resp = child_process.execSync(cmd);
+  const result = resp.toString("UTF8");
+  return result;
+}
+
+function decodeText(str) {
+  str = str.toLowerCase();
+  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng
+  str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
+  return str;
+}
+
+function transcribe(filePath) {
+  // Using template API from FPT AI
+  const command = `curl -X POST ${API_URL} -H "api-key: ${API_KEY}" -T ${filePath}`;
+
+  const result = runCmd(command);
+  console.log(result);
+  const response = JSON.parse(result);
+  if (response.hypotheses) {
+    const text = response.hypotheses[0].utterance;
+    return decodeText(text);
+  }
+  return null;
+}
+
+// Cleanup output
+fsExtra.emptyDirSync(OUTPUT_DIR);
+
+console.log(`Start transcribing "${fileName}"`);
+
+const result = transcribe(`${INPUT_DIR}/${fileName}`);
+
+const outputFileName = `${fileName.split(".")[0]}.txt`;
+fs.writeFile(`${OUTPUT_DIR}/${outputFileName}`, result, function (err) {
+  if (err) return console.log(err);
+});
+
+console.log(`Transcribe "${fileName}" completed`);
